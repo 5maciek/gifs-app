963cde5e229df3d3747a0b3b6c264b6795cf9cad
Maciej Sobol <maciej.sobol@skelia.com>
2022-10-08 17:09:45 +0200

    create GiftList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { GifList } from './components/GifList/gifList';
 import { Logo } from './components/Logo/logo';
 
 function App() {
   return (
     <div className="App">
       <Logo />
+      <GifList />
     </div >
   );
 }
---
+++ src/components/GifList/gifList.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import styles from './gifList.module.css';
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
+import useFetch, { GifData } from '../../hooks/useFetch';
+
+export const GifList = () => {
+    const [inputValue, setInputValue] = useState<string>("");
+    const [inputValueToFetch, setInputValueToFetch] = useState<string>("");
+    const { loadMoreRef, page } = useInfiniteScroll();
+    const { loading, data } = useFetch(page, inputValueToFetch);
+
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLElement>) => {
+        if (event.code === "Enter") {
+            setInputValueToFetch(inputValue)
+        }
+    }
+
+    const handleSearch = () => {
+        setInputValueToFetch(inputValue)
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles["input-container"]}>
+                <input
+                    name="search"
+                    autoComplete="off"
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInput}
+                    onKeyDown={handleKeyPress}
+                    placeholder={"search a gif"}
+                />
+                <button onClick={handleSearch}>
+                    <img src="https://img.icons8.com/ios-glyphs/30/000000/search--v1.png" />
+                </button>
+            </div>
+            <ul className={styles.box}>
+                {data.map((gif: GifData) => {
+                    return (
+                        <li key={gif.id} className={styles.card}>
+                            <img alt={gif.title} src={`${gif.images.downsized.url}`} />
+                        </li>
+                    )
+                })}
+                <div ref={loadMoreRef}>{loading ? <div className="loader"></div> : "no data more"}</div>
+            </ul>
+        </div>
+    );
+}
+
