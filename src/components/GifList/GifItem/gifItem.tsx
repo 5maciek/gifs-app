125eabc3947c9bad73000a6814b95c4a0183ee62
Maciej Sobol <maciej.sobol@skelia.com>
2022-10-08 21:10:57 +0200

    add button to copy url of gif

--- src/components/GifList/GifItem/gifItem.tsx
+++ src/components/GifList/GifItem/gifItem.tsx
@@ -1,5 +1,6 @@
 import styles from './gifItem.module.css';
 import { GifData } from '../../../hooks/useFetch';
+import { AiOutlineLink } from "react-icons/ai";
 
 export const GifItem = (props: GifData) => {
     const { id, title, images } = props;
@@ -7,6 +8,7 @@ export const GifItem = (props: GifData) => {
     return (
         <li key={id} className={styles.card}>
             <img alt={title} src={`${images.downsized.url}`} />
+            <button onClick={() => { alert("You have copied url to clipboard"); navigator.clipboard.writeText(images.downsized.url) }}><AiOutlineLink /></button>
         </li>
     );
 }
