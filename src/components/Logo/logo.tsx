283a27655dcf7da952a7c05efe2f06bfc1343acd
Maciej Sobol <maciej.sobol@skelia.com>
2022-10-08 13:22:17 +0200

    add logo component

---
+++ src/components/Logo/logo.tsx
@@ -0,0 +1,12 @@
+import styles from './logo.module.css';
+
+export const Logo = () => {
+    return (
+        <div className={styles.container}>
+            <h1>
+                GIFs
+            </h1 >
+        </div>
+    );
+}
+
