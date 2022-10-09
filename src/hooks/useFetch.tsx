bccc335dcf92bb139392b42923f74ec06f466d05
Maciej Sobol <maciej.sobol@skelia.com>
2022-10-09 12:08:56 +0200

    add useLocalStorage hook to useFetch hook

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
+import { useLocation } from "react-router-dom";
+import useLocalStorage from './useLocalStorage';
 
 const BASIC_URL = "https://api.giphy.com/v1/gifs";
 const LIMIT = 5;
-
+const SINGLE_FETCH_ROUTES = ["/random", "/favorite"];
 export interface GifData {
     id: string;
     title?: string;
@@ -13,20 +15,24 @@ export interface GifData {
     }
 }
 
-function useFetch(page: number = 1, serachText?: string, random?: boolean) {
+function useFetch(page: number = 1, serachText?: string) {
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState<GifData[]>([]);
+    const [ids] = useLocalStorage("favorite", "");
 
-    let url = `${BASIC_URL}/trending?api_key=${process.env.REACT_APP_API_KEY}&limit=${LIMIT}&offset=${page * LIMIT}&rating=G&lang=en`
+    const location = useLocation();
+
+    const OPTIONS_QUERY = `&limit=${LIMIT}&offset=${page * LIMIT}&rating=G&lang=en`;
+
+    let url = `${BASIC_URL}/trending?api_key=${process.env.REACT_APP_API_KEY}${OPTIONS_QUERY}`
 
     if (serachText) {
-        url = `${BASIC_URL}/search?api_key=${process.env.REACT_APP_API_KEY}&q=${serachText}&limit=${LIMIT}&offset=${page * LIMIT}&rating=G&lang=en`
-    }
-
-    if (random) {
+        url = `${BASIC_URL}/search?api_key=${process.env.REACT_APP_API_KEY}&q=${serachText}${OPTIONS_QUERY}`
+    } else if (location.pathname === "/random") {
         url = `${BASIC_URL}/random?api_key=${process.env.REACT_APP_API_KEY}`
+    } else if (location.pathname === "/favorite") {
+        url = `${BASIC_URL}?ids=${ids.join(",")}&api_key=${process.env.REACT_APP_API_KEY}`
     }
-
     const getData = async () => {
         try {
             setLoading(true);
@@ -35,10 +41,11 @@ function useFetch(page: number = 1, serachText?: string, random?: boolean) {
             );
             const data = await response.json();
             const mappedData = Array.isArray(data.data) ? data.data : [data.data];
-            if (random) {
+            if (SINGLE_FETCH_ROUTES.includes(location.pathname)) {
                 setData(mappedData)
+            } else {
+                setData((prev: GifData[]) => [...prev, ...mappedData]);
             }
-            setData((prev: GifData[]) => [...prev, ...mappedData]);
             setLoading(false);
         } catch (err) {
             console.error(err);
