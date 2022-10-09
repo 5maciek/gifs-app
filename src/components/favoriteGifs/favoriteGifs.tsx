f8fc1b05bdc056e3ce1eee3b570fdeedae3291f1
Maciej Sobol <maciej.sobol@skelia.com>
2022-10-09 12:08:18 +0200

    add favorites gifs component

---
+++ src/components/favoriteGifs/favoriteGifs.tsx
@@ -0,0 +1,29 @@
+import useFetch, { GifData } from "../../hooks/useFetch";
+import { GifItem } from "../gifList/gifItem/gifItem";
+import { Spinner } from "../spinner/spinner";
+import { AiOutlineDatabase } from "react-icons/ai";
+import styles from "./favoriteGifs.module.css";
+
+
+export const FavoriteGifs = () => {
+    const { loading, data } = useFetch();
+
+    return (
+        <div className={styles.container}>
+            {loading ? <Spinner /> :
+                data.length === 0 ?
+                    <div className={styles["no-favorites"]}>
+                        <AiOutlineDatabase />
+                        <p>You don't have any favorite Gifs yet</p>
+                    </div>
+                    :
+                    data.map((gif: GifData) => {
+                        return (
+                            <GifItem id={gif.id} title={gif.title} images={gif.images} />
+                        )
+                    }
+                    )}
+        </div >
+    );
+}
+
